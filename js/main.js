2f250cb315b842546d991c9730b407fcca56c0e8
Nicholas-L1n <166076016+Nicholas-L1n@users.noreply.github.com>
2024-06-08 16:33:09 -0700

    6.8.24 Push
    Still needs hide show functions for the selector sections (items, generator, closet)

--- js/main.js
+++ js/main.js
@@ -10,20 +10,55 @@ function delay(fnName, ms) {
 
 
 
-// JQUERY Event Listeners
 
 
-// page selector
+// PAGE SELECTOR
+
+// function to check which section is selected and show that section
+function checkID(element) {
+
+    // check element is pointing to what is selected
+    console.log(element)
+
+
+    if (element.id === 'upload'){
+        console.log("run upload")
+
+    } else if (element.id === 'items') {
+        console.log("run items")
+
+    } else if (element.id === 'generator') {
+        console.log("run generator")
+
+    } else if (element.id === 'closet') {
+        console.log("run closet")
+        
+    }
+}
+
 $(".menu").click(function(){
     $(".menu").removeClass("selected");
     $(this).toggleClass("selected");
+    // console.log(this)
 
+    // element I want to check (what has the selected class)
+    var element = document.querySelector('.selected.menu');
+
+    checkID(element);
 });
 
 
 
 
-// query selectors
+
+
+
+
+
+
+
+
+// Questionare selectors (bottom ui)
 
 // clothing type
 $(".typeSelect").click(function(){
