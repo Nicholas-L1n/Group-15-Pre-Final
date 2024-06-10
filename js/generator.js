3e9af1ebdde4174dfbd5f5292495db9ebad93af1
Nicholas-L1n <166076016+Nicholas-L1n@users.noreply.github.com>
2024-06-10 03:43:05 -0700

    Final Submission 000 6.10.24

--- js/generator.js
+++ js/generator.js
@@ -1,10 +1,19 @@
-var hats = [];
+var coldHats = [];
 
-var shirts = [];
+var coldShirts = [];
 
-var pants = [];
+var coldPants = [];
 
-var shoes = [];
+var coldShoes = [];
+
+var hotHats = [];
+
+var hotShirts = [];
+
+var hotPants = [];
+
+var hotShoes = [];
+
 
 
 
@@ -12,38 +21,116 @@ $("#generatorSelector").click(function() {
 
     console.log("GENERATOR SORT FUNCTION")
 
-    hats = [];
+    coldHats = [];
 
-    shirts = [];
+    coldShirts = [];
 
-    pants = [];
+    coldPants = [];
 
-    shoes = [];
+    coldShoes = [];
+
+    hotHats = [];
+
+    hotShirts = [];
+
+    hotPants = [];
+
+    hotShoes = [];
 
     for (var i=0; i<itemClosetArray.length; i++) {
         sortClothes(itemClosetArray, i);
+        
     }
 
-    console.log(hats.length, shirts.length, pants.length, shoes.length)
 });
 
+$("#generateOutfit").click(function() {
+    if (temperature == 'hot') {
+        console.log("indexing hot arrays")
 
+        indexClothingArray(hotHats, '#generatorCarousel.hat')
+
+        indexClothingArray(hotShirts, '#generatorCarousel.shirt')
+
+        indexClothingArray(hotPants, '#generatorCarousel.pants')
+
+        indexClothingArray(hotShoes, '#generatorCarousel.shoes')
+
+    } else {
+        console.log("indexing cold arrays")
+
+        indexClothingArray(coldHats, '#generatorCarousel.hat')
+
+        indexClothingArray(coldShirts, '#generatorCarousel.shirt')
+
+        indexClothingArray(coldPants, '#generatorCarousel.pants')
+
+        indexClothingArray(coldShoes, '#generatorCarousel.shoes')
+    }
+
+
+
+});
+    
 
 function sortClothes(itemClosetArray, x) {
     if (itemClosetArray.length==0) {
         return
     }
 
-    itemType = itemClosetArray[x].clothingType
+    itemType = itemClosetArray[x].clothingType;
+    itemTemperature = itemClosetArray[x].temperature;
 
-    if (itemType == 'shirt') {
-        shirts.push(itemClosetArray[x])
-    } else if (itemType == 'hat') {
-        hats.push(itemClosetArray[x])
-    } else if (itemType == 'pants') {
-        pants.push(itemClosetArray[x])
-    } else if (itemType == 'shoes') {
-        shoes.push(itemClosetArray[x])
+    if (itemTemperature == 'hot'){
+        if (itemType == 'shirt') {
+            hotShirts.push(itemClosetArray[x])
+        } else if (itemType == 'hat') {
+            hotHats.push(itemClosetArray[x])
+        } else if (itemType == 'pants') {
+            hotPants.push(itemClosetArray[x])
+        } else if (itemType == 'shoes') {
+            hotShoes.push(itemClosetArray[x])
+        }
+    } else {
+        if (itemType == 'shirt') {
+            coldShirts.push(itemClosetArray[x])
+        } else if (itemType == 'hat') {
+            coldHats.push(itemClosetArray[x])
+        } else if (itemType == 'pants') {
+            coldPants.push(itemClosetArray[x])
+        } else if (itemType == 'shoes') {
+            coldShoes.push(itemClosetArray[x])
+        }
     }
 
+
 }
+
+// random math generator based on items on input max parameter
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+// indexes array and creates an image
+function indexClothingArray(clothingArray, classAdd) {
+    if (clothingArray.length == 0) {
+        console.log("ending generation")
+        return
+    }
+
+    $(classAdd).empty();
+
+    randomIndex = getRandomInt(clothingArray.length);
+
+    imagePreview = $('<img>', {
+        src: clothingArray[randomIndex].uploadedImage
+    });
+
+    imagePreview.addClass('generatorPreview');
+
+    $(classAdd).append(imagePreview);
+
+
+    
+};
